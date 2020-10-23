ff3b54c4ef8fe7486aa660ba99b16c3b08739246
Justyn Butler <commits@justyn.co>
2020-10-23 03:38:32 +0100

    Add script to fix bottom side component rotation value in CPL
    And also adjust column names.

---
+++ cpl-process.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+import csv
+
+
+def process_cpl(in_path, out_path):
+
+    data = []
+
+    with open(in_path, newline="") as f:
+        data = list(csv.DictReader(f))
+
+    # Fix rotation for bottom sides (make negative)
+    for row in data:
+        if row["Side"] == "bottom":
+            row["Rot"] = "-" + str(row["Rot"])
+
+    # Update column names
+    column_translate = [
+        ("Val", "Part"),
+        ("Ref", "Reference Designator"),
+        ("PosX", "Center X"),
+        ("PosY", "Center Y"),
+        ("Rot", "Rotation"),
+    ]
+    for row in data:
+        for old, new in column_translate:
+            row[new] = row.pop(old)
+
+        # Delete package column
+        row.pop("Package")
+
+    with open(out_path, mode="w") as f:
+        fieldnames = [
+            "Reference Designator",
+            "Part",
+            "Center X",
+            "Center Y",
+            "Rotation",
+            "Side",
+        ]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description="Fix CPL output including bottom side rotation")
+    parser.add_argument("in_file", type=Path, help="Input CPL csv file")
+    parser.add_argument("out_file", type=Path, help="Output CPL csv file")
+
+    args = parser.parse_args()
+
+    process_cpl(args.in_file, args.out_file)
